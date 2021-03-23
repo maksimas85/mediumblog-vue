008c91669bdf6f71a9ba5e76d86d4e57327f8103
Alexandr Maximovich <asmaksimovich@gmail.com>
2021-03-24 02:03:10 +0400

    add prettirrc plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,10 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
+  extends: [
     'plugin:vue/essential',
-    'eslint:recommended'
+    'eslint:recommended',
+    "@vue/prettier"
   ],
   parserOptions: {
     parser: 'babel-eslint'
--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
   <div id="app">
-		<h1>Name Project</h1>
+    <h1>Name Project</h1>
   </div>
 </template>
